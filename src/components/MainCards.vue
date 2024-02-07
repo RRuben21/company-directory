5d45319e5de25052a3debd21e3103fb373ca8221
RRuben21 <rubenrosa.76@gmail.com>
2024-02-07 13:57:13 -0600

    Card data and style done

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -4,7 +4,9 @@ import MaincardSingle from './MaincardSingle.vue/';
 
 
 <template>
+<div class="grid grid-cols-5 gap-10 p-10">
+    <MaincardSingle v-for = "card in 20" :key="card"></MaincardSingle>
+</div>
 
-<MaincardSingle v-for = "card in 20" :key="card"></MaincardSingle>
 
 </template>
